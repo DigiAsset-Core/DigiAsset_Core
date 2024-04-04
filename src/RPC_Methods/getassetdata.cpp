b612ded02ddfa64775de4a4d15a36dca41467be0
Renzo Diaz <renzo.diaz@remadi.net>
2024-04-04 17:05:23 -0500

    -added simplified result on getassetdata

--- src/RPC_Methods/getassetdata.cpp
+++ src/RPC_Methods/getassetdata.cpp
@@ -12,11 +12,13 @@ namespace RPCMethods {
      *
      * Usage 1:
      *  params[0] - assetIndex(integer)
+     *  params[1] - simplified(boolean optional) default false
      *
      * Usage 2:
      *  params[0] - assetId(string)
      *  params[1] - txid(string optional)
      *  params[2] - vout(integer optional)
+     *  params[3] - simplified(boolean optional) default false
      *  txid and vout are for any transaction involving the asset.  These are only needed for assets that
      *  have more than 1 index.  All assets starting with L or Ua have only 1 index
      *
@@ -25,7 +27,7 @@ namespace RPCMethods {
      * ***Note DigiByte locked up in asset utxos is returned as part of DigiByte total.  Actual spendable amount will be less.
      */
     extern const Json::Value getassetdata(const Json::Value& params) {
-        if (params.size() < 1 || params.size() > 3) {
+        if (params.size() < 1 || params.size() > 4) {
             throw DigiByteException(RPC_INVALID_PARAMS, "Invalid params");
         }
 
@@ -33,7 +35,7 @@ namespace RPCMethods {
         DigiAsset asset;
 
         try {
-            if (params.size() == 3) {
+            if (params.size() >= 3) {
                 //definitely usage 2(all values included)
                 if (!params[0].isString() || !params[1].isString() || (params[1].asString().length()!=64) || !params[2].isInt()) {
                     throw DigiByteException(RPC_INVALID_PARAMS, "Invalid params");
@@ -42,7 +44,7 @@ namespace RPCMethods {
                         params[0].asString(),
                         params[1].asString(),
                         params[2].asInt()));
-            } else if (params.size() == 2) {
+            } if (params.size() == 2 && !params[1].isBool()) {
                 throw DigiByteException(RPC_INVALID_PARAMS, "Invalid params");
             } else if (params[0].isString()) {
                 asset = db->getAsset(db->getAssetIndex(params[0].asString()));
@@ -57,6 +59,21 @@ namespace RPCMethods {
 
         //look up how many assets exist
         asset.setCount(db->getTotalAssetCount(asset.getAssetIndex()));
+
+        //get simplified default is false
+        bool simplified=false;
+        if (params.size()>1) {
+            if (params[1].isBool()) {
+                simplified = params[1].asBool();
+            }
+        }
+        if (params.size()>3) {
+            if (params[3].isBool()) {
+                simplified = params[3].asBool();
+            }
+        }
+
+        if (simplified) return asset.toJSON(false, true, true);
 
         //return result
         return asset.toJSON();
